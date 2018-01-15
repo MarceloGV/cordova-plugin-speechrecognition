f3165f65663e144470eec47f240135841cce9e78
Marcelo <marcelogvallejos@gmail.com>
2018-01-15 17:27:32 -0300

    OnBusy error stop recognizer

--- src/android/com/pbakondy/SpeechRecognition.java
+++ src/android/com/pbakondy/SpeechRecognition.java
@@ -299,6 +299,8 @@ public class SpeechRecognition extends CordovaPlugin {
     public void onError(int errorCode) {
       String errorMessage = getErrorText(errorCode);
       Log.d(LOG_TAG, "Error: " + errorMessage);
+      if(SpeechRecognizer.ERROR_RECOGNIZER_BUSY)
+	      this.stop();
       callbackContext.error(errorMessage);
     }
 
