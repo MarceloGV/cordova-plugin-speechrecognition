fed7c7f687882a48558afe04925b2475eafadd93
Marcelo <marcelogvallejos@gmail.com>
2018-01-16 10:39:13 -0300

    Update SpeechRecognition.java

--- src/android/com/pbakondy/SpeechRecognition.java
+++ src/android/com/pbakondy/SpeechRecognition.java
@@ -299,8 +299,6 @@ public class SpeechRecognition extends CordovaPlugin {
     public void onError(int errorCode) {
       String errorMessage = getErrorText(errorCode);
       Log.d(LOG_TAG, "Error: " + errorMessage);
-      if(errorCode == SpeechRecognizer.ERROR_RECOGNIZER_BUSY)
-	      stop();
       callbackContext.error(errorMessage);
     }
 
